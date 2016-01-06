25c8e051dd89d0a68308bb93b31a06a644d97945
rana saimoua <ranasaimoua@hotmail.com>
2016-01-06 03:18:23 +0200

    SQLite3

--- db/migrate/20151215123225_drop_experiences.rb
+++
@@ -1,9 +0,0 @@
-class DropExperiences < ActiveRecord::Migration
-  def up
-    drop_table :experiences
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
--- db/migrate/20151215234428_add_exp_col_to_app.rb
+++
@@ -1,5 +0,0 @@
-class AddExpColToApp < ActiveRecord::Migration
-  def change
-    add_column :apps, :experience, :text
-  end
-end
